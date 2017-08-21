766be3576db18854fdf5e43bd40982032106ddfd
Ahna Hughes <ahnahughes@gmail.com>
2017-08-21 23:35:00 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,7 @@
 def introduction(name)
-  puts "Hi, my name is #{Josh}"
+  puts "Hi, my name is #{Josh}."
 end
 
-def #introduction_with_language (name); (language)
-  puts "Hi, my name is #{name} and I am learning in #{language}"
+def #introduction_with_language (name) (language)
+  puts "Hi, my name is #{name} and I am learning in #{language}."
 end
-
-puts #introduction_with_language "Ahna", "Ruby"
